59aaa7d47f0c00b4221abe8b7b1415828d710b0c
Muhammad Muaaz Saleem <muhammad.muaaz.saleem@zalando.de>
2018-09-27 13:57:12 +0200

    Using testify/require and ran dep ensure
    Using testify/require so tests terminate on failing checks. Ran
    dep ensure to add the dependency, it also updated other dependencies.
    
    Signed-off-by: Muhammad Muaaz Saleem <muhammad.muaaz.saleem@zalando.de>

--- controller/ingress_test.go
+++ controller/ingress_test.go
@@ -1,23 +1,22 @@
 package controller
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	zv1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando/v1"
+	fakeController "github.com/zalando-incubator/stackset-controller/pkg/client/clientset/versioned/fake"
+	scController "github.com/zalando-incubator/stackset-controller/pkg/clientset"
 	"k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"testing"
-
-	fakeController "github.com/zalando-incubator/stackset-controller/pkg/client/clientset/versioned/fake"
-	ScController "github.com/zalando-incubator/stackset-controller/pkg/clientset"
 	fakeK8s "k8s.io/client-go/kubernetes/fake"
+	"testing"
 )
 
 func getFakeController() *StackSetController {
 	fStacksetClient := fakeController.NewSimpleClientset()
 	fk8sClient := fakeK8s.NewSimpleClientset()
-	fSSClientSet := ScController.NewClientset(fk8sClient, fStacksetClient)
+	fSSClientSet := scController.NewClientset(fk8sClient, fStacksetClient)
 
 	return NewStackSetController(fSSClientSet, "test-controller", 0)
 }
@@ -37,22 +36,21 @@ func TestIngressCreation(t *testing.T) {
 	}
 
 	err := controller.ReconcileIngress(sc)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	ingressList, err := controller.client.ExtensionsV1beta1().Ingresses("").List(v1.ListOptions{})
-	assert.NoError(t, err)
-	ingressLength := len(ingressList.Items)
-	assert.Equal(t, 1, ingressLength)
+	require.NoError(t, err)
+	require.Len(t, ingressList.Items, 1)
 }
 
 // Test Ingress gets deleted if not defined in StackSet Spec
 func TestIngressDeletion(t *testing.T) {
 	controller := getFakeController()
 	stackIngress, err := controller.client.ExtensionsV1beta1().Ingresses("").Create(&v1beta1.Ingress{})
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	ingressList, err := controller.client.ExtensionsV1beta1().Ingresses("").List(v1.ListOptions{})
 	ingressCount := len(ingressList.Items)
-	assert.Equal(t, 1, ingressCount)
+	require.Equal(t, 1, ingressCount)
 
 	sc := StackSetContainer{
 		Ingress: stackIngress,
@@ -60,10 +58,8 @@ func TestIngressDeletion(t *testing.T) {
 
 	err = controller.ReconcileIngress(sc)
 	ingressList, err = controller.client.ExtensionsV1beta1().Ingresses("").List(v1.ListOptions{})
-	assert.NoError(t, err)
-
-	ingressCount = len(ingressList.Items)
-	assert.Equal(t, 0, ingressCount)
+	require.NoError(t, err)
+	require.Len(t, ingressList.Items, 0)
 
 }
 
@@ -94,8 +90,8 @@ func TestIngressUpdate(t *testing.T) {
 		},
 	})
 
-	assert.Equal(t, "not-example.com", stackIngress.Spec.Rules[0].Host)
-	assert.Equal(t, 8080, stackIngress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.ServicePort.IntValue())
+	require.Equal(t, "not-example.com", stackIngress.Spec.Rules[0].Host)
+	require.Equal(t, 8080, stackIngress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.ServicePort.IntValue())
 
 	sc := StackSetContainer{
 		StackContainers: map[types.UID]*StackContainer{
@@ -121,12 +117,11 @@ func TestIngressUpdate(t *testing.T) {
 	}
 
 	err = controller.ReconcileIngress(sc)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	ingressList, err := controller.client.ExtensionsV1beta1().Ingresses("default").List(v1.ListOptions{})
-	assert.NoError(t, err)
-	ingressCount := len(ingressList.Items)
-	assert.Equal(t, 1, ingressCount)
-	assert.Equal(t, "example.com", ingressList.Items[0].Spec.Rules[0].Host)
-	assert.Equal(t, 80, ingressList.Items[0].Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.ServicePort.IntValue())
+	require.NoError(t, err)
+	require.Len(t, ingressList.Items, 1)
+	require.Equal(t, "example.com", ingressList.Items[0].Spec.Rules[0].Host)
+	require.Equal(t, 80, ingressList.Items[0].Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.ServicePort.IntValue())
 }
