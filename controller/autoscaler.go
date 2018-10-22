fc63a94b52fd2fc2b72901b2c4529319099cda7d
Arjun Naik <arjun.naik@zalando.de>
2018-10-22 14:48:41 +0200

    Implemented the simplified autoscaling spec for stacksets.
    Signed-off-by: Arjun Naik <arjun.naik@zalando.de>

---
+++ controller/autoscaler.go
@@ -0,0 +1,166 @@
+package controller
+
+import (
+	"fmt"
+	zv1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando/v1"
+	autoscaling "k8s.io/api/autoscaling/v2beta1"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strconv"
+)
+
+const (
+	AmazonSQSMetricName   = "AmazonSQS"
+	PodJSONMetricName     = "PodJson"
+	IngressMetricName     = "Ingress"
+	CPUMetricName         = "CPU"
+	ZMONMetricName        = "ZMON"
+	requestsPerSecondName = "requests-per-second"
+	metricConfigJsonPath  = "metric-config.pods.%s.json-path/path"
+	metricConfigJsonKey   = "metric-config.pods.%s.json-path/json-key"
+	metricConfigJsonPort  = "metric-config.pods.%s.json-path/port"
+	SQSQueueLengthTag     = "sqs-queue-length"
+	SQSQueueNameTag       = "queue-name"
+	SQSQueueRegionTag     = "region"
+)
+
+func (c *StackSetController) ReconcileAutoscalers(container *StackSetContainer) error {
+	if container.StackSet.Spec.StackTemplate.Spec.HorizontalPodAutoscaler != nil {
+		return nil
+	}
+	autoscaler := container.StackSet.Spec.StackTemplate.Spec.Autoscaler
+	stacksetName := container.StackSet.Name
+	if autoscaler != nil {
+		var generatedHPA zv1.HorizontalPodAutoscaler
+		generatedHPA.MinReplicas = autoscaler.MinReplicas
+		generatedHPA.MaxReplicas = autoscaler.MaxReplicas
+		generatedHPA.Metrics = make([]autoscaling.MetricSpec, len(autoscaler.Metrics))
+
+		for i, m := range autoscaler.Metrics {
+			var generated autoscaling.MetricSpec
+			var err error
+			switch m.Type {
+			case AmazonSQSMetricName:
+				generated, err = SQSMetric(m)
+				if err != nil {
+					return err
+				}
+			case PodJSONMetricName:
+				g, annotations, err := PodJsonMetric(m)
+				if err != nil {
+					return err
+				}
+				generatedHPA.Annotations = make(map[string]string)
+				for k, v := range annotations {
+					generatedHPA.Annotations[k] = v
+				}
+				generated = g
+			case IngressMetricName:
+				generated, err = IngressMetric(m, stacksetName)
+				if err != nil {
+					return err
+				}
+			case ZMONMetricName:
+				return fmt.Errorf("not implemented metric type: %s", ZMONMetricName)
+			case CPUMetricName:
+				generated, err = CPUMetric(m)
+				if err != nil {
+					return err
+				}
+			default:
+				return fmt.Errorf("metric type %s not supported", m.Type)
+			}
+
+			generatedHPA.Metrics[i] = generated
+		}
+		container.StackSet.Spec.StackTemplate.Spec.HorizontalPodAutoscaler = &generatedHPA
+	}
+	return nil
+}
+
+func CPUMetric(metrics zv1.AutoscalerMetrics) (autoscaling.MetricSpec, error) {
+	if metrics.AverageUtilization == nil {
+		return autoscaling.MetricSpec{}, fmt.Errorf("utilization is not specified")
+	}
+	generated := autoscaling.MetricSpec{
+		Type: autoscaling.ResourceMetricSourceType,
+		Resource: &autoscaling.ResourceMetricSource{
+			Name:                     v1.ResourceCPU,
+			TargetAverageUtilization: metrics.AverageUtilization,
+		},
+	}
+	return generated, nil
+}
+
+func SQSMetric(metrics zv1.AutoscalerMetrics) (autoscaling.MetricSpec, error) {
+	if metrics.Average == nil {
+		return autoscaling.MetricSpec{}, fmt.Errorf("average not specified")
+	}
+	quantity, err := resource.ParseQuantity(strconv.Itoa(int(*metrics.Average)))
+	if err != nil {
+		return autoscaling.MetricSpec{}, err
+	}
+	if metrics.Queue == nil || metrics.Queue.Name == "" || metrics.Queue.Region == "" {
+		return autoscaling.MetricSpec{}, fmt.Errorf("queue not specified correctly")
+	}
+	generated := autoscaling.MetricSpec{
+		Type: autoscaling.ExternalMetricSourceType,
+		External: &autoscaling.ExternalMetricSource{
+			MetricName: SQSQueueLengthTag,
+			MetricSelector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{SQSQueueNameTag: metrics.Queue.Name, SQSQueueRegionTag: metrics.Queue.Region},
+			},
+			TargetAverageValue: &quantity,
+		},
+	}
+	return generated, nil
+}
+
+func PodJsonMetric(metrics zv1.AutoscalerMetrics) (autoscaling.MetricSpec, map[string]string, error) {
+	if metrics.Average == nil {
+		return autoscaling.MetricSpec{}, nil, fmt.Errorf("average is not specified for metric")
+	}
+	quantity, err := resource.ParseQuantity(strconv.Itoa(int(*metrics.Average)))
+	if err != nil {
+		return autoscaling.MetricSpec{}, nil, err
+	}
+	generated := autoscaling.MetricSpec{
+		Type: autoscaling.PodsMetricSourceType,
+		Pods: &autoscaling.PodsMetricSource{
+			MetricName:         metrics.Endpoint.Name,
+			TargetAverageValue: quantity,
+		},
+	}
+	if metrics.Endpoint == nil || metrics.Endpoint.Port == 0 || metrics.Endpoint.Path == "" || metrics.Endpoint.Key == "" || metrics.Endpoint.Name == "" {
+		return autoscaling.MetricSpec{}, nil, fmt.Errorf("the metrics endpoint is not specified correctly")
+	}
+	annotations := map[string]string{
+		fmt.Sprintf(metricConfigJsonKey, metrics.Endpoint.Name):  metrics.Endpoint.Key,
+		fmt.Sprintf(metricConfigJsonPath, metrics.Endpoint.Name): metrics.Endpoint.Path,
+		fmt.Sprintf(metricConfigJsonPort, metrics.Endpoint.Name): strconv.Itoa(int(metrics.Endpoint.Port)),
+	}
+	return generated, annotations, nil
+}
+func IngressMetric(metrics zv1.AutoscalerMetrics, ingressName string) (autoscaling.MetricSpec, error) {
+	if metrics.Average == nil {
+		return autoscaling.MetricSpec{}, fmt.Errorf("average value not specified for metric")
+	}
+	quantity, err := resource.ParseQuantity(strconv.Itoa(int(*metrics.Average)))
+	if err != nil {
+		return autoscaling.MetricSpec{}, err
+	}
+	generated := autoscaling.MetricSpec{
+		Type: autoscaling.ObjectMetricSourceType,
+		Object: &autoscaling.ObjectMetricSource{
+			MetricName: requestsPerSecondName,
+			Target: autoscaling.CrossVersionObjectReference{
+				APIVersion: "extensions/v1beta1",
+				Kind:       "Ingress",
+				Name:       ingressName,
+			},
+			TargetValue: quantity,
+		},
+	}
+	return generated, nil
+}
---
+++ controller/autoscaler_test.go
@@ -0,0 +1,202 @@
+package controller
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/zalando-incubator/stackset-controller/pkg/apis/zalando/v1"
+	zv1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando/v1"
+	"k8s.io/api/autoscaling/v2beta1"
+	v12 "k8s.io/api/core/v1"
+	"testing"
+)
+
+func generateSSAutoscalerStub(minReplicas, maxReplicas int32) *StackSetContainer {
+	container := &StackSetContainer{
+		StackSet: zv1.StackSet{
+			Spec: zv1.StackSetSpec{
+				StackTemplate: zv1.StackTemplate{
+					Spec: zv1.StackSpecTemplate{
+						StackSpec: zv1.StackSpec{
+							Autoscaler: &zv1.Autoscaler{
+								MinReplicas: &minReplicas,
+								MaxReplicas: maxReplicas,
+								Metrics: []zv1.AutoscalerMetrics{
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return container
+}
+
+func generateSSAutoscalerCPU(minReplicas, maxReplicas, utilization int32) *StackSetContainer {
+	container := generateSSAutoscalerStub(minReplicas, maxReplicas)
+	container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics = append(
+		container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics, zv1.AutoscalerMetrics{
+			Type:               CPUMetricName,
+			AverageUtilization: &utilization,
+		})
+	return container
+}
+
+func generateSSAutoscalerSQS(minReplicas, maxReplicas, utilization int32, queueName, queueRegion string) *StackSetContainer {
+	container := generateSSAutoscalerStub(minReplicas, maxReplicas)
+	container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics = append(
+		container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics, zv1.AutoscalerMetrics{
+			Type: AmazonSQSMetricName,
+			Queue: &zv1.MetricsQueue{
+				Name:   queueName,
+				Region: queueRegion,
+			},
+			Average: &utilization,
+		},
+	)
+	return container
+}
+
+func generateSSAutoscalerPodJson(minReplicas, maxReplicas, utilization, port int32, name, path, key string) *StackSetContainer {
+	container := generateSSAutoscalerStub(minReplicas, maxReplicas)
+	container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics = append(
+		container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics, zv1.AutoscalerMetrics{
+			Type: PodJSONMetricName,
+			Endpoint: &zv1.MetricsEndpoint{
+				Path: path,
+				Name: name,
+				Key:  key,
+				Port: port,
+			},
+			Average: &utilization,
+		},
+	)
+	return container
+}
+func generateSSAutoscalerIngress(minReplicas, maxReplicas, utilization int32) *StackSetContainer {
+	container := generateSSAutoscalerStub(minReplicas, maxReplicas)
+	container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics = append(
+		container.StackSet.Spec.StackTemplate.Spec.Autoscaler.Metrics, zv1.AutoscalerMetrics{
+			Type:    IngressMetricName,
+			Average: &utilization,
+		},
+	)
+	return container
+}
+
+func TestStackSetController_ReconcileAutoscalersCPU(t *testing.T) {
+	controller := StackSetController{}
+	container := generateSSAutoscalerCPU(1, 10, 80)
+	err := controller.ReconcileAutoscalers(container)
+	assert.NoError(t, err, "failed to reconcile autoscaler")
+	hpa := container.StackSet.Spec.StackTemplate.Spec.HorizontalPodAutoscaler
+	assert.NotNil(t, hpa, "hpa not generated")
+	assert.Equal(t, int32(1), *hpa.MinReplicas, "min replicas not generated correctly")
+	assert.Equal(t, int32(10), hpa.MaxReplicas, "max replicas generated incorrectly")
+	assert.Len(t, hpa.Metrics, 1, "expected HPA to have 1 metric. instead got %d", len(hpa.Metrics))
+	cpuMetric := hpa.Metrics[0]
+	assert.Equal(t, cpuMetric.Type, v2beta1.ResourceMetricSourceType)
+	assert.Equal(t, cpuMetric.Resource.Name, v12.ResourceCPU)
+	assert.Equal(t, *cpuMetric.Resource.TargetAverageUtilization, int32(80))
+}
+
+func TestStackSetController_ReconcileAutoscalersSQS(t *testing.T) {
+	controller := StackSetController{}
+	container := generateSSAutoscalerSQS(1, 10, 80, "test-queue", "test-region")
+	err := controller.ReconcileAutoscalers(container)
+	assert.NoError(t, err, "failed to reconcile autoscaler")
+	hpa := container.StackSet.Spec.StackTemplate.Spec.HorizontalPodAutoscaler
+	assert.NotNil(t, hpa, "hpa not generated")
+	assert.Equal(t, int32(1), *hpa.MinReplicas, "min replicas not generated correctly")
+	assert.Equal(t, int32(10), hpa.MaxReplicas, "max replicas generated incorrectly")
+	assert.Len(t, hpa.Metrics, 1, "expected HPA to have 1 metric. instead got %d", len(hpa.Metrics))
+	externalMetric := hpa.Metrics[0]
+	assert.Equal(t, externalMetric.Type, v2beta1.ExternalMetricSourceType)
+	assert.Equal(t, externalMetric.External.MetricName, "sqs-queue-length")
+	assert.Equal(t, externalMetric.External.MetricSelector.MatchLabels["queue-name"], "test-queue")
+	assert.Equal(t, externalMetric.External.MetricSelector.MatchLabels["queue-name"], "test-queue")
+	assert.Equal(t, externalMetric.External.TargetAverageValue.Value(), int64(80))
+}
+
+func TestStackSetController_ReconcileAutoscalersPodJson(t *testing.T) {
+	controller := StackSetController{}
+	container := generateSSAutoscalerPodJson(1, 10, 80, 8080, "current-load", "/metrics", "$.current-load.counter")
+	err := controller.ReconcileAutoscalers(container)
+	assert.NoError(t, err, "failed to reconcile autoscaler")
+	hpa := container.StackSet.Spec.StackTemplate.Spec.HorizontalPodAutoscaler
+	assert.NotNil(t, hpa, "hpa not generated")
+	assert.Equal(t, int32(1), *hpa.MinReplicas, "min replicas not generated correctly")
+	assert.Equal(t, int32(10), hpa.MaxReplicas, "max replicas generated incorrectly")
+	assert.Len(t, hpa.Metrics, 1, "expected HPA to have 1 metric. instead got %d", len(hpa.Metrics))
+	podMetrics := hpa.Metrics[0]
+	assert.Equal(t, podMetrics.Type, v2beta1.PodsMetricSourceType)
+	assert.Equal(t, hpa.Annotations["metric-config.pods.current-load.json-path/json-key"], "$.current-load.counter")
+	assert.Equal(t, hpa.Annotations["metric-config.pods.current-load.json-path/path"], "/metrics")
+	assert.Equal(t, hpa.Annotations["metric-config.pods.current-load.json-path/port"], "8080")
+	assert.Equal(t, podMetrics.Pods.TargetAverageValue.Value(), int64(80))
+	assert.Equal(t, podMetrics.Pods.MetricName, "current-load")
+}
+func TestStackSetController_ReconcileAutoscalersIngress(t *testing.T) {
+	controller := StackSetController{}
+	container := generateSSAutoscalerIngress(1, 10, 80)
+	err := controller.ReconcileAutoscalers(container)
+	assert.NoError(t, err, "failed to reconcile autoscaler")
+	hpa := container.StackSet.Spec.StackTemplate.Spec.HorizontalPodAutoscaler
+	assert.NotNil(t, hpa, "hpa not generated")
+	assert.Equal(t, int32(1), *hpa.MinReplicas, "min replicas not generated correctly")
+	assert.Equal(t, int32(10), hpa.MaxReplicas, "max replicas generated incorrectly")
+	assert.Len(t, hpa.Metrics, 1, "expected HPA to have 1 metric. instead got %d", len(hpa.Metrics))
+	ingressMetrics := hpa.Metrics[0]
+	assert.Equal(t, ingressMetrics.Type, v2beta1.ObjectMetricSourceType)
+	assert.Equal(t, ingressMetrics.Object.TargetValue.Value(), int64(80))
+	assert.Equal(t, ingressMetrics.Object.MetricName, "requests-per-second")
+}
+
+func TestCPUMetricValid(t *testing.T) {
+	var utilization int32 = 80
+	metrics := v1.AutoscalerMetrics{Type: "cpu", AverageUtilization: &utilization}
+	metric, err := CPUMetric(metrics)
+	assert.NoError(t, err, "could not create hpa metric")
+	assert.Equal(t, metric.Resource.Name, v12.ResourceCPU)
+}
+
+func TestCPUMetricInValid(t *testing.T) {
+	metrics := v1.AutoscalerMetrics{Type: "cpu", AverageUtilization: nil}
+	_, err := CPUMetric(metrics)
+	assert.Error(t, err, "created metric even when utilization not specified")
+}
+
+func TestPodJsonMetricInvalid(t *testing.T) {
+	endpoints := []zv1.MetricsEndpoint{
+		{
+			Path: "/metrics",
+			Port: 8080,
+			Key:  "$.metrics_key",
+		},
+		{
+			Path: "/metrics",
+			Port: 8080,
+			Name: "metric-name",
+		},
+		{
+			Path: "/metrics",
+			Key:  "$.metrics_key",
+			Name: "metric-name",
+		},
+		{
+			Port: 8080,
+			Key:  "$.metrics_key",
+			Name: "metric-name",
+		},
+	}
+	for _, e := range endpoints {
+		metrics := v1.AutoscalerMetrics{Type: PodJSONMetricName, Endpoint: &e}
+		_, _, err := PodJsonMetric(metrics)
+		assert.Error(t, err, "created metric with invalid configuration")
+	}
+}
+
+func TestIngressMetricInvalid(t *testing.T) {
+	metrics := v1.AutoscalerMetrics{Type: IngressMetricName, Average: nil}
+	_, err := IngressMetric(metrics, "stack-name")
+	assert.Errorf(t, err, "created metric with invalid configuration")
+}
