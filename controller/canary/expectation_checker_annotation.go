1e3aec2a2c929aa2ae90958f39832784ab8df3fb
Thibaut Le Page <thibaut.le.page@zalando.de>
2019-05-16 15:15:39 +0200

    canary: annotationChecker: v1 -> zv1 for readability
    Signed-off-by: Thibaut Le Page <thibaut.le.page@zalando.de>

--- controller/canary/expectation_checker_annotation.go
+++ controller/canary/expectation_checker_annotation.go
@@ -1,18 +1,18 @@
 package canary
 
-import v1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando.org/v1"
+import zv1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando.org/v1"
 
 // annotationChecker is a very basic ExpectationChecker.
 // It considers expectations fulfilled if and only if the green stack does not have
-// the annotationCheckerKey annotation, which is not removed.
+// the annotationCheckerKey annotation (which is not removed if it exists).
 // Handy for manual experiments, but do not use in production:
-// it probably is not be desirable to continue a gradual deployment when the service responsible
-// for adding the annotationCheckerKey annotation is down.
+// it probably is not desirable to continue a gradual deployment when the service responsible
+// for adding the annotation is down.
 type annotationChecker struct{}
 
 const annotationCheckerKey = canaryAnnotationKeyPrefix + "expectations-not-fulfilled"
 
-func (c annotationChecker) ExpectationsFulfilled(green, _ v1.Stack) bool {
+func (c annotationChecker) ExpectationsFulfilled(green, _ zv1.Stack) bool {
 	_, ok := green.Annotations[annotationCheckerKey]
 	return !ok
 }
