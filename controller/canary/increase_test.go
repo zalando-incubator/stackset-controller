35f9fee1eed868ba1623d9cbac6e44957b91365d
Rahul De <rahul080327@gmail.com>
2019-05-16 16:10:49 +0200

    Add actual tests for increase
    Signed-off-by: Rahul De <rahul080327@gmail.com>

--- controller/canary/increase_test.go
+++ controller/canary/increase_test.go
@@ -2,17 +2,30 @@ package canary
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/zalando-incubator/stackset-controller/controller/canary/percent"
 	"github.com/zalando-incubator/stackset-controller/controller/entities"
 )
 
 func Test_Unchanged_GreenTraffic(t *testing.T) {
 	trafficMap := TrafficMap{
 		"A": entities.NewTrafficStatusSame(100),
-		"B": entities.NewTrafficStatusSame(100),
-		"C": entities.NewTrafficStatusSame(100),
+		"B": entities.NewTrafficStatusSame(0),
+		"C": entities.NewTrafficStatusSame(0),
 	}
 
-	assert.Equal(t, trafficMap, trafficMap)
+	newTraffic, _ := increaseTraffic(
+		trafficMap,
+		"A",
+		"B",
+		"C",
+		*percent.NewFromInt(95),
+		*percent.NewFromInt(95),
+		time.Millisecond,
+		time.Millisecond,
+	)
+
+	assert.Equal(t, trafficMap, newTraffic)
 }
