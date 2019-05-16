6ada179226a74cad9c06023eba1b2b6eee12139e
Thibaut Le Page <thibaut.le.page@zalando.de>
2019-05-16 15:06:27 +0200

    canary: document annotationChecker
    Signed-off-by: Thibaut Le Page <thibaut.le.page@zalando.de>

--- controller/canary/expectation_checker_annotation.go
+++ controller/canary/expectation_checker_annotation.go
@@ -2,6 +2,12 @@ package canary
 
 import v1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando.org/v1"
 
+// annotationChecker is a very basic ExpectationChecker.
+// It considers expectations fulfilled if and only if the green stack does not have
+// the annotationCheckerKey annotation, which is not removed.
+// Handy for manual experiments, but do not use in production:
+// it probably is not be desirable to continue a gradual deployment when the service responsible
+// for adding the annotationCheckerKey annotation is down.
 type annotationChecker struct{}
 
 const annotationCheckerKey = canaryAnnotationKeyPrefix + "expectations-not-fulfilled"
