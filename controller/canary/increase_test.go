4ee025b2c7637fc3559c28e9ad4bf78abde1268c
Rahul De <rahul080327@gmail.com>
2019-05-16 17:29:46 +0200

    Add phase1 test
    Signed-off-by: Rahul De <rahul080327@gmail.com>

--- controller/canary/increase_test.go
+++ controller/canary/increase_test.go
@@ -29,3 +29,30 @@ func Test_Unchanged_GreenTraffic(t *testing.T) {
 
 	assert.Equal(t, trafficMap, newTraffic)
 }
+
+func Test_Phase1(t *testing.T) {
+	trafficMap := TrafficMap{
+		"A": entities.NewTrafficStatusSame(0),
+		"B": entities.NewTrafficStatusSame(55),
+		"C": entities.NewTrafficStatusSame(45),
+	}
+
+	expectedMap := TrafficMap{
+		"A": entities.NewTrafficStatusSame(0),
+		"B": entities.NewTrafficStatus(55, 50),
+		"C": entities.NewTrafficStatus(45, 50),
+	}
+
+	newTraffic, _ := increaseTraffic(
+		trafficMap,
+		"A",
+		"B",
+		"C",
+		*percent.NewFromInt(45),
+		*percent.NewFromInt(100),
+		2 * 60 * 1000 * time.Millisecond,
+		1 * 60 * 1000 * time.Millisecond,
+	)
+
+	assert.Equal(t, expectedMap, newTraffic)
+}
