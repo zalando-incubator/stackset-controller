ce660adb2a11b6802e79f3914d5483121ff93064
Sandor Szücs <sandor.szuecs@zalando.de>
2019-10-08 14:23:56 +0200

    add external ingress based traffic switch
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>
    
    fix: shorten stackset names to fix dns entries
    
    Signed-off-by: Martin Linkhorst <martin.linkhorst@zalando.de>

--- cmd/e2e/traffic_switch_test.go
+++ cmd/e2e/traffic_switch_test.go
@@ -160,7 +160,7 @@ func TestTrafficSwitchStackset(t *testing.T) {
 func TestTrafficSwitchDesiredStacksetActualIngress(t *testing.T) {
 	t.Parallel()
 
-	stacksetName := "switch-traffic-desired-stackset-actual-ing"
+	stacksetName := "switch-traffic-des-stackset-act-ing"
 	firstVersion := "v1"
 	firstStack := fmt.Sprintf("%s-%s", stacksetName, firstVersion)
 	updatedVersion := "v2"
@@ -235,7 +235,7 @@ func TestTrafficSwitchDesiredStacksetActualIngress(t *testing.T) {
 func TestTrafficSwitchDesiredIngressActualStackset(t *testing.T) {
 	t.Parallel()
 
-	stacksetName := "switch-traffic-desired-ing-actual-stackset"
+	stacksetName := "switch-traffic-des-ing-act-stackset"
 	firstVersion := "v1"
 	firstStack := fmt.Sprintf("%s-%s", stacksetName, firstVersion)
 	updatedVersion := "v2"
@@ -291,6 +291,78 @@ func TestTrafficSwitchDesiredIngressActualStackset(t *testing.T) {
 	// Switch traffic 0/100
 	newDesiredWeights := map[string]float64{updatedStack: 100}
 	err = setDesiredTrafficWeightsIngress(stacksetName, newDesiredWeights)
+	require.NoError(t, err)
+	err = trafficWeightsUpdatedStackset(t, stacksetName, weightKindActual, newDesiredWeights, nil).await()
+	require.NoError(t, err)
+
+	err = stackStatusMatches(t, firstStack, expectedStackStatus{
+		actualTrafficWeight:  pfloat64(0),
+		desiredTrafficWeight: pfloat64(0),
+	}).await()
+	require.NoError(t, err)
+	err = stackStatusMatches(t, updatedStack, expectedStackStatus{
+		actualTrafficWeight:  pfloat64(100),
+		desiredTrafficWeight: pfloat64(100),
+	}).await()
+	require.NoError(t, err)
+}
+
+func TestTrafficSwitchStacksetExternalIngress(t *testing.T) {
+	t.Parallel()
+
+	stacksetName := "switch-traffic-stackset-external"
+	firstVersion := "v1"
+	firstStack := fmt.Sprintf("%s-%s", stacksetName, firstVersion)
+	updatedVersion := "v2"
+	updatedStack := fmt.Sprintf("%s-%s", stacksetName, updatedVersion)
+	factory := NewTestStacksetSpecFactory(stacksetName).ExternalIngress()
+	spec := factory.Create(firstVersion)
+	err := createStackSet(stacksetName, 0, spec)
+	require.NoError(t, err)
+	_, err = waitForStack(t, stacksetName, firstVersion)
+	require.NoError(t, err)
+	spec = factory.Create(updatedVersion)
+	err = updateStackset(stacksetName, spec)
+	require.NoError(t, err)
+	_, err = waitForStack(t, stacksetName, updatedVersion)
+	require.NoError(t, err)
+
+	initialWeights := map[string]float64{firstStack: 100}
+	err = trafficWeightsUpdatedStackset(t, stacksetName, weightKindActual, initialWeights, nil).await()
+	require.NoError(t, err)
+
+	err = stackStatusMatches(t, firstStack, expectedStackStatus{
+		actualTrafficWeight:  pfloat64(100),
+		desiredTrafficWeight: pfloat64(100),
+	}).await()
+	require.NoError(t, err)
+	err = stackStatusMatches(t, updatedStack, expectedStackStatus{
+		actualTrafficWeight:  pfloat64(0),
+		desiredTrafficWeight: pfloat64(0),
+	}).await()
+	require.NoError(t, err)
+
+	// Switch traffic 50/50
+	desiredWeights := map[string]float64{firstStack: 50, updatedStack: 50}
+	err = setDesiredTrafficWeightsStackset(stacksetName, desiredWeights)
+	require.NoError(t, err)
+	err = trafficWeightsUpdatedStackset(t, stacksetName, weightKindActual, desiredWeights, nil).await()
+	require.NoError(t, err)
+
+	err = stackStatusMatches(t, firstStack, expectedStackStatus{
+		actualTrafficWeight:  pfloat64(50),
+		desiredTrafficWeight: pfloat64(50),
+	}).await()
+	require.NoError(t, err)
+	err = stackStatusMatches(t, updatedStack, expectedStackStatus{
+		actualTrafficWeight:  pfloat64(50),
+		desiredTrafficWeight: pfloat64(50),
+	}).await()
+	require.NoError(t, err)
+
+	// Switch traffic 0/100
+	newDesiredWeights := map[string]float64{updatedStack: 100}
+	err = setDesiredTrafficWeightsStackset(stacksetName, newDesiredWeights)
 	require.NoError(t, err)
 	err = trafficWeightsUpdatedStackset(t, stacksetName, weightKindActual, newDesiredWeights, nil).await()
 	require.NoError(t, err)
