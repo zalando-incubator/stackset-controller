7f9848c3ed99c3b20aacb5d3d7c85295d90717dc
Muaaz Saleem <muhammad.muaaz.saleem@zalando.de>
2020-08-27 13:14:28 +0200

    Testing that Autoscaling defaults are configured

--- cmd/e2e/configure_hpa_test.go
+++ cmd/e2e/configure_hpa_test.go
@@ -31,8 +31,8 @@ func TestConfigureHPA(t *testing.T) {
 	require.EqualValues(t, stabilizationWindow, *hpa.Spec.Behavior.ScaleDown.StabilizationWindowSeconds)
 }
 
-// TestBehaviorDefaults tests defaults are applied to HPA Behavior
-func TestBehaviorDefaults(t *testing.T) {
+// TestHPABehaviorDefaults tests defaults are applied to HPA Behavior
+func TestHPABehaviorDefaults(t *testing.T) {
 	t.Parallel()
 	stacksetName := "hpa-behavior"
 	factory := NewTestStacksetSpecFactory(stacksetName).
@@ -89,5 +89,43 @@ func TestConfigureAutoscaling(t *testing.T) {
 	fullFirstName := fmt.Sprintf("%s-%s", stacksetName, firstVersion)
 	hpa, err := waitForHPA(t, fullFirstName)
 	require.NoError(t, err)
+
+	require.NotNil(t, hpa.Spec.Behavior.ScaleDown.StabilizationWindowSeconds, "HPA StabilizationWindowSeconds is nil")
 	require.EqualValues(t, stabilizationWindow, *hpa.Spec.Behavior.ScaleDown.StabilizationWindowSeconds)
+}
+
+// TestAutoscalingDefaults tests defaults are applied to Autoscaling Behavior
+func TestAutoscalingDefaults(t *testing.T) {
+	t.Parallel()
+	stacksetName := "configured-autoscaler"
+	metrics := []zv1.AutoscalerMetrics{
+		makeCPUAutoscalerMetrics(50),
+		makeExternalAutoscalerMetrics("test", "eu-central-1", 10),
+		makeObjectAutoscalerMetrics(20),
+	}
+	require.Len(t, metrics, 3)
+
+	factory := NewTestStacksetSpecFactory(stacksetName).
+		Ingress().
+		Autoscaler(1, 10, metrics)
+
+	firstVersion := "v1"
+	spec := factory.Create(firstVersion)
+	scalePolicy := autoscalingv2beta2.MaxPolicySelect
+	spec.StackTemplate.Spec.Autoscaler.Behavior =
+		&autoscalingv2beta2.HorizontalPodAutoscalerBehavior{
+			ScaleDown: &autoscalingv2beta2.HPAScalingRules{
+				SelectPolicy: &scalePolicy,
+			},
+		}
+
+	err := createStackSet(stacksetName, 0, spec)
+	require.NoError(t, err)
+
+	fullFirstName := fmt.Sprintf("%s-%s", stacksetName, firstVersion)
+	hpa, err := waitForHPA(t, fullFirstName)
+	require.NoError(t, err)
+
+	require.NotNil(t, hpa.Spec.Behavior.ScaleDown.StabilizationWindowSeconds, "Autoscaler StabilizationWindowSeconds is nil")
+	require.EqualValues(t, 300, *hpa.Spec.Behavior.ScaleDown.StabilizationWindowSeconds)
 }
