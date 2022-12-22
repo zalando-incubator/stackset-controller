69fe992a06b5289f12469a17252a4fcaa8248aa4
Katyanna Moura <amelie.kn@gmail.com>
2022-12-22 11:18:24 +0100

    Update config fixes

--- cmd/e2e/test_utils.go
+++ cmd/e2e/test_utils.go
@@ -62,7 +62,7 @@ var (
 					},
 				},
 				ReadinessProbe: &corev1.Probe{
-					Handler: corev1.Handler{
+					ProbeHandler: corev1.ProbeHandler{
 						HTTPGet: &corev1.HTTPGetAction{
 							Path:   "/",
 							Port:   intstr.FromInt(80),
