424c532be6fb72d6b6b2881bc3bf57ce0b0f2082
Milad Barazandeh <miladbrznd@gmail.com>
2023-10-23 14:40:10 +0200

    Remove HPA when stacket is scaled down (#538)
    Co-authored-by: Milad Barazandeh <milad.barazandeh@zalando.de>

--- pkg/core/stack_resources.go
+++ pkg/core/stack_resources.go
@@ -223,6 +223,10 @@ func (sc *StackContainer) GenerateHPA() (*autoscaling.HorizontalPodAutoscaler, e
 		return nil, nil
 	}
 
+	if sc.ScaledDown() {
+		return nil, nil
+	}
+
 	result := &autoscaling.HorizontalPodAutoscaler{
 		ObjectMeta: sc.resourceMeta(),
 		TypeMeta: metav1.TypeMeta{
