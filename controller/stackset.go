b3037d0ec0e425782a6cc790a73014c74f7430fa
Katyanna Moura <amelie.kn@gmail.com>
2024-02-05 13:15:59 +0100

    review: drier
    Signed-off-by: Katyanna Moura <amelie.kn@gmail.com>

--- controller/stackset.go
+++ controller/stackset.go
@@ -45,8 +45,6 @@ const (
 	defaultResetMinReplicasDelay = 10 * time.Minute
 )
 
-var configurationResourceNameError = "ConfigurationResource name must be prefixed by Stack name. ConfigurationResource: %s, Stack: %s"
-
 // StackSetController is the main controller. It watches for changes to
 // stackset resources and starts and maintains other controllers per
 // stackset resource.
@@ -1513,9 +1511,7 @@ func validateAllConfigurationResourcesNames(stack *zv1.Stack) error {
 			rscName = rsc.SecretRef.Name
 		}
 
-		if !strings.HasPrefix(rscName, stack.Name) {
-			return fmt.Errorf(configurationResourceNameError, rscName, stack.Name)
-		}
+		return validateConfigurationResourceName(stack.Name, rscName)
 	}
 	return nil
 }
@@ -1524,7 +1520,8 @@ func validateAllConfigurationResourcesNames(stack *zv1.Stack) error {
 // name is not prefixed by Stack name.
 func validateConfigurationResourceName(stack string, rsc string) error {
 	if !strings.HasPrefix(rsc, stack) {
-		return fmt.Errorf(configurationResourceNameError, rsc, stack)
+		return fmt.Errorf("ConfigurationResource name must be prefixed by Stack name."+
+			"ConfigurationResource: %s, Stack: %s", rsc, stack)
 	}
 	return nil
 }
