5d46c2d3b8c86592fd6cdbffb033096049535094
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2024-02-19 17:06:48 +0100

    Drop old autoscaling api code
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- cmd/e2e/generated_autoscaler_test.go
+++ cmd/e2e/generated_autoscaler_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	zv1 "github.com/zalando-incubator/stackset-controller/pkg/apis/zalando.org/v1"
-	"k8s.io/api/autoscaling/v2beta1"
+	v2 "k8s.io/api/autoscaling/v2"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
 
@@ -55,12 +55,12 @@ func TestGenerateAutoscaler(t *testing.T) {
 	require.EqualValues(t, 10, hpa.Spec.MaxReplicas)
 	require.Len(t, hpa.Spec.Metrics, 3)
 	metric1 := hpa.Spec.Metrics[0]
-	require.EqualValues(t, metric1.Type, v2beta1.ExternalMetricSourceType)
+	require.EqualValues(t, metric1.Type, v2.ExternalMetricSourceType)
 	require.EqualValues(t, metric1.External.Target.AverageValue.Value(), 10)
 	metric2 := hpa.Spec.Metrics[1]
-	require.EqualValues(t, v2beta1.ObjectMetricSourceType, metric2.Type)
+	require.EqualValues(t, v2.ObjectMetricSourceType, metric2.Type)
 	require.EqualValues(t, 20, metric2.Object.Target.AverageValue.Value())
 	metric3 := hpa.Spec.Metrics[2]
-	require.EqualValues(t, v2beta1.ResourceMetricSourceType, metric3.Type)
+	require.EqualValues(t, v2.ResourceMetricSourceType, metric3.Type)
 	require.EqualValues(t, 50, *metric3.Resource.Target.AverageUtilization)
 }
